209e9b0e2b7bab4625383ef11758a8c263eb85eb
Jonas H <pjh.hellstrom@gmail.com>
2019-07-07 15:31:50 -0400

    finalized functionality and score tracking, app is functional with layout mostly finished

--- assets/javascript/wordGuessGame.js
+++ assets/javascript/wordGuessGame.js
@@ -1,78 +1,121 @@
-// Create constant array of random words, guessing array
-const countries = ["CANADA", "USA", "MEXICO"];
+// Create constant array of random words to guess
+const cities = ["OTTAWA", "TORONTO", "MONTREAL", "QUEBEC", "CALGARY", "EDMONTON", "VANCOUVER", "VICTORIA", "HALIFAX", "WINNIPEG", "SASKATOON", "REGINA", "WHITEHORSE", "YELLOWKNIFE"];
+var playerScore = 0;
+var wrongLetters = [];
+var wrongCounter = 0;
 
-
-// When player clicks start...
+// When player clicks start button...
 function playGame() {
 
 // Pick a word randomly from words-array and set as secret word
-var secretWord = countries[Math.floor(Math.random() * countries.length)];
+var secretWord = cities[Math.floor(Math.random() * cities.length)];
 
 // Split secret word into individual letters
 var secretWordLetters = secretWord.split("");
-console.log(secretWordLetters);
 
-// Create guessing outline based on array length
+// Create guessing outline based on array length and show on screen
 var wordOutline = [];
-for(var ol = 0; ol < secretWordLetters.length; ol++) {
-    wordOutline.push("_ ");
-}// end for-loop
+    for(var ol = 0; ol < secretWordLetters.length; ol++) {
+        wordOutline.push("_ ");
+    }// end for-loop
+
+    document.getElementById("wordOutline").innerHTML = wordOutline;
 
 // Create counter to track letters left to guess
 var lettersRemaining = secretWord.length;
 
-// Main function 
+// Get player guess on key press
 
-var wrongLetters = [];
-var wrongCounter = 0;
-console.log(lettersRemaining);
-// debugger;
-
-// Get player guess
-if (lettersRemaining < 0) {
-
-    alert("WINNER!");
-    return;
-
-}//end main if-loop
-
-else if (lettersRemaining > 0) {
     document.onkeydown = function getGuess (e) {
-        var letter = String.fromCharCode(e.which);
-        console.log(letter);
+
+    console.log(secretWord);
+
+
+
+
+
+    var letter = String.fromCharCode(e.which);
+
+    // Check if letters remain to be guessed
+    if (lettersRemaining > 1) {
+        
+
+
+        //Check if letter has been guessed already
+        if (wordOutline.includes(letter) == true || wrongLetters.includes(letter) == true) {
+            alert("you've already guessed this letter!");
+            return;
+        }//end if-loop
+        
+        // As long as letter has not been guessed already 
+        // proceed to check if letter is in secret word
+        else {
+        // Check if guess is in SecretWordLetters array using for-loop with nested if - update result on screen
+            for (var i = 0; i < secretWordLetters.length; i++) {
+
+                if (secretWordLetters.includes(letter) == false) {  
+                    wrongLetters.push(letter);
+                    wrongCounter++;
+                    if (wrongCounter >4) {
+                        letter = [];
+                        playerScore = 0;
+                        wrongCounter = 0;
+                        wrongLetters = [];
+                        document.getElementById("wrongLetters").innerHTML = wrongLetters;
+                        document.getElementById("scoreTracker").innerHTML = playerScore + "/5";
+                        alert("Oops, too many wrong guesses... The city we were looking for is " + secretWord);  
+                        secretWord = []; 
+                        playGame();     
+                    }//end if
+                    else {
+                    document.getElementById("wrongLetters").innerHTML = wrongLetters;
+                    return;
+                    }//end else
+                }// end outer if
+
+                else if (letter===secretWordLetters[i]) {
+                    wordOutline[i] = letter;
+                    document.getElementById("wordOutline").innerHTML = wordOutline;
+                    lettersRemaining--;
+                }//end else if
+
+            }//end for-loop
+        
+        }//end else
     
-    //Check if letter has been guessed already
-    if (wordOutline.includes(letter) == true || wrongLetters.includes(letter) == true) {
-        alert("you've already guessed this letter!");
-        return;
-    }
-    
-    // As long as letter has not been guessed already 
-    // proceed to check if letter is in secret word
+    } //end if-loop
+
     else {
-    // Check if guess is in SecretWordLetters array using for-loop and if
-        for (var i = 0; i < secretWordLetters.length; i++) {
-            if (secretWordLetters.includes(letter) == false) {
-                wrongLetters.push(letter);
-                wrongCounter++;
-                console.log("wrongCounter: "+ wrongCounter);
-                console.log("wrongLetters: "+ wrongLetters);
-                return;
-            }
-            else if (letter===secretWordLetters[i]) {
-                console.log("name: " + secretWordLetters[i]); //replace with push to correct array
-                wordOutline[i] = letter;
-                // wordOutline.splice(i, i, letter);
-                lettersRemaining--;
-                console.log("wordOutline: " + wordOutline);
-            }//end else if
-        }//end for-loop
-    
-        console.log(lettersRemaining)
-    
-    }//end else
-    
+        // increase score and update to screen
+        playerScore++; 
+        //Check if score greater than 4, if it is alert winner and reset
+        if (playerScore >4) {
+            letter = [];
+            secretWord = [];
+            playerScore = 0;
+            wrongCounter = 0;
+            wrongLetters = [];
+            document.getElementById("wrongLetters").innerHTML = wrongLetters;
+            document.getElementById("scoreTracker").innerHTML = playerScore + "/5";   
+            alert("Good job! You're a winner!");
+            playGame();     
+        }//end if
+        else {
+            document.getElementById("scoreTracker").innerHTML = playerScore + "/5";   
+            // reset letters remaining counter, wrong counter and wrong letter range, update to screen
+            lettersRemaining = 0;
+            wrongCounter = 0;
+            wrongLetters = [];
+            document.getElementById("wrongLetters").innerHTML = wrongLetters;
+
+            // call playGame() for a new round
+            playGame();
+        }//end else
+    }//end outer else
     }//end getGuess function
-}// end main else if function
 
 }//end function playGame
+
+
+                    // //Check if wrongCount is higher than 4, if it is, reset score and launch a new game
+
